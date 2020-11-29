57ba353f436dde19b8d6af201f808fe19d5fc049
Steffen Andersland <stefandersland@gmail.com>
2020-11-28 23:30:26 -0500

    implemented crud for employees

--- src/routes/employee.route.ts
+++ src/routes/employee.route.ts
@@ -3,7 +3,7 @@ import {Router} from 'express';
 import Employee from '../models/Employee.model';
 import {NotFoundError} from "../utils";
 
-const employees = [
+const employees: Employee[] = [
   new Employee("Steffen", "Andersland")
 ];
 
@@ -17,9 +17,43 @@ router.get("/:id", (req, res) => {
   const employeeId = req.params.id;
   const employee = employees.find((employee) => employee.id === employeeId);
   if (!employee) {
-    throw new NotFoundError(`No Employee found with ID ${employeeId}`);
+    throw new NotFoundError(`No employee found with ID ${employeeId}.`);
   }
-  res.json({message: "success", payload: employee});
+  res.json({status: "success", payload: employee});
+});
+
+router.post("/", (req, res) => {
+  const {firstName, lastName} = (req.body as {firstName: string, lastName: string});
+  const employee = new Employee(firstName, lastName);
+  employees.push(employee);
+  res.json({status: "success", payload: employee});
+});
+
+router.patch("/:id", (req, res) => {
+  const employeeId = req.params.id;
+  const employeeIdx = employees.findIndex((employee) => employee.id === employeeId);
+  if (employeeIdx === -1) {
+    throw new NotFoundError(`No employee found with ID ${employeeId}.`);
+  }
+  
+  const newEmployee = Object.entries(req.body as {fistName: string, lastName: string})
+    .reduce((employee, [key, value]) => {
+      return Object.assign(employee, {[key]: value});
+    }, employees[employeeIdx]);
+
+  employees[employeeIdx] = newEmployee;
+  res.json({status: "success", payload: newEmployee});
+});
+
+router.delete("/:id", (req, res) => {
+  const employeeId = req.params.id;
+  const employeeIdx = employees.findIndex((employee) => employee.id === employeeId);
+  if (employeeIdx === -1) {
+    throw new NotFoundError(`No employee found with ID ${employeeId}.`);
+  }
+  
+  employees.splice(employeeIdx, 1);
+  res.json({status: "success", message: `employee ${employeeId} deleted.`}); 
 });
 
 export default router;
